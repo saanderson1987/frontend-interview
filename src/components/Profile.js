425726d9b08c82d8d71057286f5f211d8b32a898
Steven Anderson <saanderson1987@gmail.com>
2020-05-20 10:29:21 -0400

    fix Profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,32 +1,37 @@
-import * as React from "react";
-import { useUserContext } from "../contexts/user";
+import React, { useState } from "react";
+import { useUserContext, useSetUserContext } from "../contexts/user";
 
 /**
  * This should update the user context with the new values for email and name
  */
 const Profile = () => {
   const user = useUserContext();
+  const setUserContext = useSetUserContext();
+
+  const [email, setEmail] = useState(user.email);
+  const [name, setName] = useState(user.name);
 
   return (
     <div>
       <h1>Edit your profile</h1>
       <form
-        onSubmit={e => {
+        onSubmit={(e) => {
           e.preventDefault();
+          setUserContext({ name, email });
         }}
       >
         <input
           name="email"
-          value={user.email}
-          onChange={e => {
-            user.email = e.target.value;
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
           }}
         />
         <input
           name="name"
-          value={user.name}
-          onChange={e => {
-            user.name = e.target.value;
+          value={name}
+          onChange={(e) => {
+            setName(e.target.value);
           }}
         />
         <button type="submit">Submit</button>
